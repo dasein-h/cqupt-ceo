60cd07977c2ed70afbb917f97989d39b515b6a9d
Rabb1nate <1542515237@qq.com>
2020-11-14 23:41:31 +0800

    可以发送参数

--- src/redux/reducers/reducer.js
+++ src/redux/reducers/reducer.js
@@ -5,18 +5,18 @@ const newstate = {
 export default  (state = newstate,action)=>{
     switch(action.type) {
         case "getAllCompanies":
-            return Object.assign({},state,action.payload);
+            return Object.assign({},state,action);
         case "login":
-            return Object.assign({},state,action.payload);
+            return Object.assign({},state,action);
         case "Login_Success":
-            return Object.assign({isLogin:true},state,action.payload);
+            return Object.assign({isLogin:true},state,action);
         case "Login_Fail":
-            return Object.assign({isLogin:false},state,action.payload);
+            return Object.assign({isLogin:false},state,action);
         case "Login_Check_OK":
-            return Object.assign({isLogin:true},state,action.payload);
+            return Object.assign({isLogin:true},state,action);
             case "Login_Check_NO":
-            return Object.assign({isLogin:false},state,action.payload);
+            return Object.assign({isLogin:false},state,action);
         default:
-            return Object.assign({},state,action.payload);
+            return Object.assign({},state,action);
     }
 }
--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -4,6 +4,7 @@ import actions from '../actionCreators/creators'
 export default function* defSaga(){
     yield throttle(2000,'login',function*(){
                 const action = yield select();
+                console.log("saga"+'aaaaaa'+action)
                 const res = yield call(Service.post,'/login/user', action.payload)
             if (res.status==200){
                 yield put(actions.Login_Success(res.message,res.data))
