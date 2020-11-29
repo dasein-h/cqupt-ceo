ce015ac3a7f35b0fde3dea06bdc4d59093fea71e
starwarship <2408205158@qq.com>
2020-11-29 20:40:28 +0800

    teacher

--- src/pages/teacher/view/Teacher.js
+++ src/pages/teacher/view/Teacher.js
@@ -11,18 +11,15 @@ import StuInfo from './StuInfo';
 import VotSit from './VotSit';
 import NewsCom from './news'
 import '../../teacher/style/contentNav.css';
+import {  Menu } from 'antd';
+import { UserOutlined, VideoCameraOutlined, EditOutlined, OrderedListOutlined } from '@ant-design/icons';
 
 class Teacher extends Component { 
     constructor(props) {
         super(props);
-        this.handleNavStyChanStu = this.handleNavStyChanStu.bind(this);
-        this.handleNavStyChanCom = this.handleNavStyChanCom.bind(this);
-        this.handleNavStyChanVot = this.handleNavStyChanVot.bind(this);
+        
         this.state = {
-            displayStu:true,
-            displayCom:true,
-           displayVot:true,
-           displayNews:true
+            
           }
     }
     render() {
@@ -30,41 +27,28 @@ class Teacher extends Component {
             <Router>
                 <div id="All">
                     <div className="nav-div">
-                        <Link to="/Teacher"> 
-                            <div className={this.state.displayStu ? 'nav' : 'nav-point'}
-                                onClick={this.handleNavStyChanStu}>
-                            学生信息
-                            </div>
-                        </Link>
-                        
-                        <Link to="/Teacher/ComInfo">
-                            <div className={this.state.displayCom ? 'nav' : 'nav-point'}
-                            onClick={this.handleNavStyChanCom}>
-                            公司情况
-                            </div>
-                        </Link>
-                        
-                        <Link to="/Teacher/VotSit">
-                            <div className={this.state.displayVot ? 'nav' : 'nav-point'}
-                            onClick={this.handleNavStyChanVot}>
-                            投票情况
-                            </div>
-                            
-                        </Link>
-                        <Link to="/Teacher/news">
-                        <div className={this.state.displayNews ? 'nav' : 'nav-point'}
-                            onClick={this.handleNavStyChanNews}>
-                            消息
-                            </div>
-                        </Link>
+                        <Menu theme="light" mode="inline">
+                            <Menu.Item key="1" icon={<UserOutlined />}>
+                                <Link to="/Teacher/StuInfo">学生信息</Link>
+                            </Menu.Item>
+                            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                                <Link to="/Teacher/ComInfo">公司情况</Link> 
+                            </Menu.Item>
+                            <Menu.Item key="3" icon={<EditOutlined />}>
+                                <Link to="/Teacher/VotSit">投票情况</Link> 
+                            </Menu.Item>
+                            <Menu.Item key="4" icon={<OrderedListOutlined />}>
+                                <Link to="/Teacher/news">消息</Link> 
+                            </Menu.Item>
+
+                        </Menu>
                         
                     </div>
 
 
                     <div className="content">
                         <Switch>
-                            {/* <Redirect component={NewsCom}></Redirect> */}
-                            <Route exact path="/Teacher">
+                            <Route exact path="/Teacher/StuInfo">
                                 <StuInfo/>
                             </Route>
                             <Route path="/Teacher/ComInfo">
@@ -73,46 +57,16 @@ class Teacher extends Component {
                             <Route path="/Teacher/VotSit">
                                 <VotSit/>
                             </Route>
-                            <Route path="/Teacher/news" component={NewsCom}></Route>
+                            <Route path="/Teacher/news">
+                                  <NewsCom/>  
+                            </Route>
                         </Switch>
                     </div>
                     
                 </div>
             </Router>
-        )
-    }
 
-    handleNavStyChanStu() { 
-            this.setState({
-                displayStu:false,
-                displayCom:true,
-                displayVot:true,
-                displayNews:true
-            })
-    }
-    handleNavStyChanCom() { 
-        this.setState({
-            displayStu:true,
-            displayCom:false,
-            displayVot:true,
-            displayNews:true
-        })
-    }
-    handleNavStyChanVot() { 
-        this.setState({
-            displayStu:true,
-            displayCom:true,
-            displayVot:false,
-            displayNews:true
-        })
-    }
-    handleNavStyChanNews = () =>{ 
-        this.setState({
-            displayStu:true,
-            displayCom:true,
-            displayVot:true,
-            displayNews:false
-        })
+        )
     }
 
 }
