245df7e69ddafe88f234b571efb007bc5ed07843
yy <you@example.com>
2020-12-01 19:21:06 +0800

    teacher

--- src/pages/teacher/components/ChosenStuClass.jsx
+++ src/pages/teacher/components/ChosenStuClass.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Table } from 'antd'; 
+import {selectedClassTeacher} from "../../../until/api/teacherApi";
 
 const columns = [
   {
@@ -11,6 +12,15 @@ const columns = [
     title: 'age',
     dataIndex: 'age',
     key: 'age',
+  },
+  {
+    title: 'age',
+    dataIndex: 'age',
+    key: 'ag',
+  },{
+    title: 'age',
+    dataIndex: 'age',
+    key: 'ae',
   },
 ];
 
@@ -28,13 +38,33 @@ const dataSource = [
 ];
 
 class ChosenStuClass extends Component{
+    constructor(prop){
+      super(prop);
+      this.state = {
+        contentList:""
+      }
+    }
     render() {
         return (
         <div>
-            <Table dataSource={dataSource} columns={columns}  rowSelection={{type:"checkbox"}}/>
+            <Table dataSource={this.state.contentList} columns={columns}/>
         </div>
   );
 };
+
+    componentDidMount(){
+      let repro = selectedClassTeacher("1","1");
+      repro.then((res) => {
+        this.setState(
+          this.state.contentList = res.data.data
+        )
+        console.log(res.data.data);
+      },(err) => {
+        console.log(err);
+      })
+
+      console.log(repro);
+    }
 }
 
 export default ChosenStuClass;
