673856f3b22d3ad2352a0a7eb7eb93d22c535614
starwarship <2408205158@qq.com>
2020-12-07 15:38:52 +0800

    管理员

---
+++ src/pages/manager/view/ImData.jsx
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react'
+
+class ImData extends Component {
+   
+    render() { 
+        return (
+            <Fragment>ImData</Fragment>
+            
+        )
+    }
+}
+export default ImData
