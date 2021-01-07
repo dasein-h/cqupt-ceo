aaa6033d194e35d715dbcc4c2a74182cab6b1d6a
FY1010 <1114550440@qq.com>
2021-01-07 18:13:50 +0800

    update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   
   app.use(createProxyMiddleware('/api', {
-    target: 'http://120.79.207.60:8089',
+    target: 'http://39.100.140.143',
     secure: false,
     changeOrigin: true,
     pathRewrite: {
