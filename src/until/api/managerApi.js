697ec47749a6b7ad656adeb1424f6d21dbd774d6
yy <you@example.com>
2021-01-08 21:35:42 +0800

    M

--- src/until/api/managerApi.js
+++ src/until/api/managerApi.js
@@ -20,6 +20,13 @@ const ManagerApi = {
         list
       }
       return Service.post('/admin/addclass', queryData)
+    },
+
+    searchTeacher: (value,currentPage) => {
+      return Service.post('/teacher/searchTeacher', {
+        value,
+      currentPage
+    })
     }
 }
 
